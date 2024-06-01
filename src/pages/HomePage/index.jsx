9a4fb5a4fb38c4058d1729825da855edbef59e18
Alice Veselá <alice.hladikova@gmail.com>
2024-06-01 10:21:54 +0200

    vytvoreni komponenty Joke

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './style.css';
+import { Joke } from '../../components/joke';
 
 export const HomePage = () => {
   const [likes, setLikes] = useState(0);
@@ -12,41 +13,7 @@ export const HomePage = () => {
   };
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={increaseLike}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {likes}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={decreaseLike}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {dislikes}
-          </span>
-        </div>
-      </div>
+      <Joke></Joke>
     </div>
   );
 };
